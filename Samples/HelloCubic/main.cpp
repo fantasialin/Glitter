ae0b961d2c8cbf660e6eed3f63b2d17525c4f182
fantasia <fantasialin@gmail.com>
2020-03-22 22:11:16 +0800

    remove redundant projection seting code.

--- Samples/HelloCubic/main.cpp
+++ Samples/HelloCubic/main.cpp
@@ -239,26 +239,28 @@ int main(int argc, char * argv[]) {
               
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 
+        glm::mat4 projection    = glm::mat4(1.0f);
+        projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        ourShader.setMat4("projection", projection);
+
         if(ObjectPositionState!=0){
             for (unsigned int i = 0; i < 10; i++){
                 // calculate the model matrix for each object and pass it to shader before drawing
                 glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
                 glm::mat4 view          = glm::mat4(1.0f);
-                glm::mat4 projection    = glm::mat4(1.0f);
+                
                 model = glm::translate(model, cubePositions[i]);
                 float angle = 20.0f * i;
                 if(ObjectScaleState!=0){
                     model = glm::rotate(model, (float)glm::radians((glfwGetTime()*angle)+glfwGetTime()), glm::vec3(0.5f, 1.0f, 0.0f));
                     view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-                    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+                    
                 }
                 else{
                     model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
                     view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-                    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
                 }
-                ourShader.setMat4("view", view);
-                ourShader.setMat4("projection", projection);
+                ourShader.setMat4("view", view);              
                 ourShader.setMat4("model", model);
 
                 glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -272,16 +274,13 @@ int main(int argc, char * argv[]) {
             if(ObjectScaleState!=0){
                 model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
                 view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-                projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
             }
             else{
                 model = glm::rotate(model, (float)glm::radians(45.0f), glm::vec3(0.5f, 1.0f, 0.0f));
                 view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-                projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
             }
             ourShader.setMat4("model", model);
             ourShader.setMat4("view", view);
-            ourShader.setMat4("projection", projection);
 
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
